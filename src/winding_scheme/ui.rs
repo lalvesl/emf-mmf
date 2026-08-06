092c0a5a56aa2bb5dc7bf1c05dfe8d33d0d21f6e
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2026-08-06 13:43:31 -0300

    refactor: update winding scheme UI to use generic toggle_row and localized string keys

--- src/winding_scheme/ui.rs
+++ src/winding_scheme/ui.rs
@@ -1,20 +1,14 @@
 use crate::config::MotorConfig;
-use crate::i18n::{Language, t};
+use crate::i18n::Strings;
+use crate::ui::toggle_row;
 use bevy_egui::egui;
+use i18n::t;
 
-pub fn winding_scheme_ui(ui: &mut egui::Ui, config: &mut MotorConfig, lang: &Language) -> bool {
-    let mut changed = false;
-
-    if ui
-        .checkbox(
-            &mut config.show_winding_scheme,
-            t(lang, "show_winding_scheme"),
-        )
-        .on_hover_text(t(lang, "show_winding_scheme_hover"))
-        .changed()
-    {
-        changed = true;
-    }
-
-    changed
+pub fn winding_scheme_ui(ui: &mut egui::Ui, config: &mut MotorConfig) -> bool {
+    toggle_row(
+        ui,
+        &mut config.show_winding_scheme,
+        &t!(Strings::ShowWindingScheme),
+        Some(&t!(Strings::ShowWindingSchemeHover)),
+    )
 }
