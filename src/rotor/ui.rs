2b849bea90c9c7fe7d3bad118e1156dfc35f7ad9
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2026-08-06 13:43:12 -0300

    refactor: unify UI styling with ShadcnTheme and modernize i18n implementation across winding and rotor components

--- src/rotor/ui.rs
+++ src/rotor/ui.rs
@@ -1,16 +1,9 @@
 use crate::config::MotorConfig;
-use crate::i18n::{Language, t};
+use crate::i18n::Strings;
+use crate::ui::toggle_row;
 use bevy_egui::egui;
+use i18n::t;
 
-pub fn rotor_ui(ui: &mut egui::Ui, config: &mut MotorConfig, lang: &Language) -> bool {
-    let mut changed = false;
-
-    if ui
-        .checkbox(&mut config.show_rotor, t(lang, "show_rotor"))
-        .changed()
-    {
-        changed = true;
-    }
-
-    changed
+pub fn rotor_ui(ui: &mut egui::Ui, config: &mut MotorConfig) -> bool {
+    toggle_row(ui, &mut config.show_rotor, &t!(Strings::ShowRotor), None)
 }
