fce18eba4459d6fce1f142257ff44344d88d2ab6
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2026-08-06 15:39:22 -0300

    refactor: decouple visualization toggle state from motor configuration by introducing ViewConfig

--- src/rotor/ui.rs
+++ src/rotor/ui.rs
@@ -1,9 +1,9 @@
-use crate::config::MotorConfig;
+use crate::config::ViewConfig;
 use crate::i18n::Strings;
 use crate::ui::toggle_row;
 use bevy_egui::egui;
 use i18n::t;
 
-pub fn rotor_ui(ui: &mut egui::Ui, config: &mut MotorConfig) -> bool {
-    toggle_row(ui, &mut config.show_rotor, &t!(Strings::ShowRotor), None)
+pub fn rotor_ui(ui: &mut egui::Ui, view: &mut ViewConfig) {
+    toggle_row(ui, &mut view.show_rotor, &t!(Strings::ShowRotor), None);
 }
